3862e08302d7a4349c9de9872676b6cb681196a2
Kovid Rathee <kovid.rathee@protonmail.com>
2024-08-13 13:07:56 +1000

    testing dark mode

--- blog.config.js
+++ blog.config.js
@@ -6,9 +6,9 @@ const BLOG = {
   description: 'My blog.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
-  appearance: 'auto', // ['light', 'dark', 'auto'],
+  appearance: 'dark', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
-  lightBackground: '#fffcf2', // use hex value, don't forget '#' e.g #fffefc
+  lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
   darkBackground: '#18181B', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
   since: 2022, // If leave this empty, current year will be used.
